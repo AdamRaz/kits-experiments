d2ea4fb6275b2a02d9dfbc430c846045889f51e6
Adam Razzak <a.abdel@hotmail.co.uk>
2022-01-08 00:24:20 +0000

    create first animation

---
+++ js/app.js
@@ -0,0 +1,11 @@
+let variableText = document.querySelector('.variable-text');
+let textArray = ["&#x0643;&#x062A;&#x0633", ]
+console.log(variableText.innerHTML);
+function repeatedCode () {
+    variableText.innerText += " & (き:つ)";
+}
+function clearText () {
+    variableText.innerText = "(ki:ts)";
+}
+let intervalID = setInterval(repeatedCode, 1500);
+let intervalID2 = setInterval(clearText, 9000);
